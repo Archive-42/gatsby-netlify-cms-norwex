4cce1e2a867b8e5287ff4aa563c1d27b61e86aff
Stackbit Code Editor <projects@stackbit.com>
2021-06-17 22:48:24 +0000

    Footer.js: changed

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -14,7 +14,7 @@ const Footer = class extends React.Component {
         <div className="content has-text-centered">
           <img
             src={logo}
-            alt="Kaldi"
+            alt="Norwex "
             style={{ width: '14em', height: '10em' }}
           />
         </div>
